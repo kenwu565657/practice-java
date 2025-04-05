1f8d00c76c836650a48c162035ea8e514d52b8f0
kenwu565657 <kenwu565656@icloud.com>
2025-04-05 18:18:00 +0800

    add effective java

---
+++ effective-java/src/main/java/com/practice/java/number4/UtilityClass.java
@@ -0,0 +1,10 @@
+package com.practice.java.number4;
+
+/**
+ * copy from effective java
+ */
+public class UtilityClass {
+    private UtilityClass() {
+        throw new AssertionError();
+    }
+}
